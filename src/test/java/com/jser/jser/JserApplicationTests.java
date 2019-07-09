3aae11662274a010dbd6c41972bf8b9ab8b4427b
John Smith <xiaosshuaii@gmail.com>
2019-07-09 22:09:18 +0800

    feat(init):

---
+++ src/test/java/com/jser/jser/JserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jser.jser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
